4f964b67b99e2a2b68f4dbf3ed24ec7873ed6684
Jaroslaw Jantura <jjantura@gmail.com>
2019-01-21 20:20:22 +0100

    Add functional interface test

--- java-8-update-exam-preparation/src/main/java/com/example/FunctionalInterface.java
+++ java-8-update-exam-preparation/src/main/java/com/example/FunctionalInterface.java
@@ -1,4 +1,16 @@
 package com.example;
 
+import static java.lang.System.out;
+
+@java.lang.FunctionalInterface
+interface MyInterface {
+    void foo();
+}
+// exactly one abstract method is allowed
+
+
 public class FunctionalInterface {
+    public static void main(String... args) {
+
+    }
 }
